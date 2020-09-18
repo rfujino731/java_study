d515744ef63bc9ce0c67f567196b74ebed3333a4
rfujino731 <wbhp.litw.twba.lib@gmail.com>
2020-09-18 19:16:59 +0900

    finish udemy java_introductory section3

---
+++ src/java_study/Staffinfo.java
@@ -0,0 +1,24 @@
+package java_study;
+
+//Staffクラスを用意する
+class Staff{
+	String name;
+	int staffid;
+	String email;
+	
+	public void sayhello() {
+		System.out.println("Hello");
+	}
+}
+public class Staffinfo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Staff yamada = new Staff();
+		yamada.name = "taro yamada";
+		
+		yamada.sayhello();
+
+	}
+
+}
--- src/java_study/practice_class.java
+++ src/java_study/practice_class.java
@@ -4,7 +4,7 @@ public class practice_class {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("こんにちは！javaプログラミング");
+		System.out.println("縺薙ｓ縺ｫ縺｡縺ｯJAVA繝励Ο繧ｰ繝ｩ繝溘Φ繧ｰ");
 
 	}
 
