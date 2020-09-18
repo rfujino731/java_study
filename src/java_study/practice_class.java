c90b162b60559d52835efbc6f7cc9f53e15d27ad
rfujino731 <wbhp.litw.twba.lib@gmail.com>
2020-09-18 17:02:38 +0900

    install Eclipse

---
+++ src/java_study/practice_class.java
@@ -0,0 +1,11 @@
+package java_study;
+
+public class practice_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("こんにちは！javaプログラミング");
+
+	}
+
+}
