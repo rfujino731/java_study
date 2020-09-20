207f13ecb50af40099349cd52d2c2452bd8ee2cd
rfujino731 <wbhp.litw.twba.lib@gmail.com>
2020-09-20 19:17:00 +0900

    studied_constructor

--- src/java_study/Staffinfo.java
+++ src/java_study/Staffinfo.java
@@ -9,13 +9,21 @@ class Staff{
 	public void sayhello() {
 		System.out.println("Hello" + this.name);
 	}
+
+	public Staff(String name, int staffid, String email) {
+		super();
+		this.name = name;
+		this.staffid = staffid;
+		this.email = email;
+	}
 }
+
 public class Staffinfo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Staff yamada = new Staff();
-		yamada.name = "taro yamada";
+		Staff yamada = new Staff("Taro Yamafa", 123, "yamada@java.com");
+		//yamada.name = "taro yamada";
 		
 		yamada.sayhello();
 
