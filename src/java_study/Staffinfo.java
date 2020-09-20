0b0de9d53b2c87afbe249058bbe05a7ccd1cfff1
rfujino731 <wbhp.litw.twba.lib@gmail.com>
2020-09-20 20:27:27 +0900

    studied getter and setter

--- src/java_study/Staffinfo.java
+++ src/java_study/Staffinfo.java
@@ -16,6 +16,30 @@ class Staff{
 		this.staffid = staffid;
 		this.email = email;
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getStaffid() {
+		return staffid;
+	}
+
+	public void setStaffid(int staffid) {
+		this.staffid = staffid;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
 
 public class Staffinfo {
@@ -25,7 +49,12 @@ public class Staffinfo {
 		Staff yamada = new Staff("Taro Yamafa", 123, "yamada@java.com");
 		//yamada.name = "taro yamada";
 		
-		yamada.sayhello();
+		//yamada.sayhello();
+		
+		System.out.println("[社員情報]");
+		System.out.println("氏名: " + yamada.getName());
+		System.out.println("社員番号: " + yamada.getStaffid());
+		System.out.println("Email: " + yamada.getEmail());
 
 	}
 
