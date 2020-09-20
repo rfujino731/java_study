1ed3bb9f443b49d3913644177d7b0ada709232a3
rfujino731 <wbhp.litw.twba.lib@gmail.com>
2020-09-20 20:40:18 +0900

    studied class_inheritance

--- src/java_study/Staffinfo.java
+++ src/java_study/Staffinfo.java
@@ -42,11 +42,23 @@ class Staff{
 	}
 }
 
+class RemoteStaff extends Staff{
+	
+	public String location;
+
+	public RemoteStaff(String name, int staffid, String email) {
+		super(name, staffid, email);
+		// TODO Auto-generated constructor stub
+	}
+}
+
 public class Staffinfo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Staff yamada = new Staff("Taro Yamafa", 123, "yamada@java.com");
+		RemoteStaff tanaka = new RemoteStaff("Hanako Tanaka", 124,"tanaka@java.com");
+		tanaka.location = "大阪";
 		//yamada.name = "taro yamada";
 		
 		//yamada.sayhello();
@@ -55,6 +67,12 @@ public class Staffinfo {
 		System.out.println("氏名: " + yamada.getName());
 		System.out.println("社員番号: " + yamada.getStaffid());
 		System.out.println("Email: " + yamada.getEmail());
+		
+		System.out.println("[社員情報]");
+		System.out.println("氏名: " + tanaka.getName());
+		System.out.println("社員番号: " + tanaka.getStaffid());
+		System.out.println("Email: " + tanaka.getEmail());
+		System.out.println("勤務地: " + tanaka.location);
 
 	}
 
